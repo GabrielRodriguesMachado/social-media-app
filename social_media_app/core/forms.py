bfe634728697f5715de49c99cb72408963352331
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-05-24 15:59:18 -0300

    login criado e funcionando

--- social_media_app/core/forms.py
+++ social_media_app/core/forms.py
@@ -1,8 +1,28 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 CLASS_INPUT = "w-full py-4 px-6 rounded-xl"
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Your username",
+                "class": CLASS_INPUT,
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Your password",
+                "class": CLASS_INPUT,
+            }
+        )
+    )
 
 
 class SignupForm(UserCreationForm):
