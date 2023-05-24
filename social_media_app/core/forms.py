7c16c6654f6dbca36ce1fa5584bb44260dd478c0
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-05-24 15:46:00 -0300

    signup criado e funcionando

---
+++ social_media_app/core/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+CLASS_INPUT = "w-full py-4 px-6 rounded-xl"
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Your usarname",
+                "class": CLASS_INPUT,
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "Your email",
+                "class": CLASS_INPUT,
+            }
+        )
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Your password",
+                "class": CLASS_INPUT,
+            }
+        )
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Confirm your password",
+                "class": CLASS_INPUT,
+            }
+        )
+    )
--- social_media_app/core/views.py
+++ social_media_app/core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Count
 from post.models import Post
+
+from .forms import SignupForm
 
 
 def index(request):
@@ -11,3 +13,16 @@ def index(request):
 
 def contact(request):
     return render(request, "core/contact.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect("/login/")
+    else:
+        form = SignupForm()
+
+    return render(request, "core/signup.html", {"form": form})
