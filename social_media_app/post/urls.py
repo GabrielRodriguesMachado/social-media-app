f4da4fb31a5dc03a5067b8009d523792bf420f36
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-05-24 15:11:04 -0300

    criado o necessário para postar um comentário

--- social_media_app/post/urls.py
+++ social_media_app/post/urls.py
@@ -7,4 +7,5 @@ app_name = "post"
 
 urlpatterns = [
     path("<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/add_comment/", views.add_comment, name="add_comment"),
 ]
--- social_media_app/post/views.py
+++ social_media_app/post/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
+from .forms import CommentForm
 
 
 def detail(request, pk):
@@ -9,3 +11,21 @@ def detail(request, pk):
     return render(
         request, "post/detail.html", {"post": post, "comments": comments}
     )
+
+
+@login_required
+def add_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.user = request.user
+            comment.save()
+            return redirect("post:detail", pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return redirect("post:detail", pk=post.pk)
