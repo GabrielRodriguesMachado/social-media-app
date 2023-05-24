f9f656c0b03a841cfb224de29a1ea509521a0188
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-05-24 10:29:20 -0300

    criada página de detalhes do post

--- social_media_app/core/tests.py
+++ social_media_app/core/tests.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from post.models import Post
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser",
+            email="test@example.com",
+            password="testpassword",
+        )
+        self.post = Post.objects.create(
+            created_by=self.user, content="Test content"
+        )
+
+    def test_str_representation(self):
+        self.assertEqual(
+            str(self.post),
+            f"Posted by {self.user.username}",
+        )
+
+
+class PostViewsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser",
+            email="test@example.com",
+            password="testpassword",
+        )
+        self.post = Post.objects.create(
+            created_by=self.user, content="Test content"
+        )
+
+    def test_contact_view(self):
+        response = self.client.get(reverse("contact"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "core/contact.html")
--- social_media_app/post/views.py
+++ social_media_app/post/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "post/detail.html", {"post": post})
